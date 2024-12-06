6be74768501ae63b89938761a522c4feed7af8d9
xuanphuda <xuanphuda@gmail.com>
2024-12-06 22:25:03 +0700

    Add KeyFeatureSection component and handle its dark mode

--- components/key-features/key-features.tsx
+++ components/key-features/key-features.tsx
@@ -1,8 +1,11 @@
+
+import Image from "next/image";
+import { realTimeTrackingProperties } from "@/utils/links";
 import { Button } from "../ui/button";
 
 const KeyFeaturesSection = () => {
     return (
-        <section className="pt-10 w-full">
+        <section className="py-10 w-full">
             <p className="text-[16px] text-[#3fa477] font-semibold">
                 Real-time Tracking
             </p>
@@ -18,6 +21,43 @@ const KeyFeaturesSection = () => {
             >
                 Learn More
             </Button>
+            <ul className="mt-8 grid grid-cols-1 lg:grid-cols-2 gap-6">
+                {realTimeTrackingProperties.map((realTimeTrackingProperty) => (
+                    <li key={realTimeTrackingProperty.key} className="w-full p-4 bg-[#faf8f6] dark:bg-[#161616] rounded-lg">
+                        <h3 className="text-[18px] text-black dark:text-white font-bold">
+                            {realTimeTrackingProperty.heading3}
+                        </h3>
+                        <ul className="mt-4 flex flex-col md:flex-row md:justify-between gap-2">
+                            <li className="text-[14px] flex gap-2">
+                                <div className="w-3 h-3 rounded-full border-2 border-[#9CDD8A]"></div>
+                                {realTimeTrackingProperty.descForHeading3[0]}
+                            </li>
+                            <li className="text-[14px] flex gap-2">
+                                <div className="w-3 h-3 rounded-full border-2 border-[#9CDD8A]"></div>
+                                {realTimeTrackingProperty.descForHeading3[1]}
+                            </li>
+                        </ul>
+                        <div className="dark:hidden mt-4">
+                            <Image
+                                src={realTimeTrackingProperty.img as string}
+                                alt="Image"
+                                className="w-full h-full"
+                                width={294}
+                                height={147}
+                            />
+                        </div>
+                        <div className="hidden dark:block mt-4">
+                            <Image
+                                src={realTimeTrackingProperty.darkImg as string}
+                                alt="Image"
+                                className="w-full h-full"
+                                width={294}
+                                height={147}
+                            />
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </section>
     )
 }
