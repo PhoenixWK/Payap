c6a1f2f70b7809ebca57d68c297aabff4e0fc05a
xuanphuda <xuanphuda@gmail.com>
2025-03-31 23:38:21 +0700

    Commit

--- components/auth-form/signup-form/signup-form.tsx
+++ components/auth-form/signup-form/signup-form.tsx
@@ -6,7 +6,7 @@ export default function SignUpForm() {
             <div>
                 <div>
                     <label htmlFor="email" className="block text-lg font-semibold text-gray-600 dark:text-[#6d6d6d] mb-1">
-                        Email<span className="text-red-500">*</span>
+                        Email<span className="text-red-500">**</span>
                     </label>
                     <input
                         type="email"
